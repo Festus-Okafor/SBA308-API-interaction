4779138e17662758c739abfbc0f68443887f09a5
festus_okafor <okaforfestus465@gmail.com>
2025-06-05 17:44:16 -0400

    updating

--- files.js
+++ files.js
@@ -1,12 +1,16 @@
 
+
     let image = document.querySelector("#image")
     let wifeName = document.querySelector("#name")
     let wifeDescription = document.querySelector("#description")
     let btn = document.querySelector("#btn")
- 
+  
+  
+  
     btn.addEventListener("click",()=>{
         location.reload();
     })
+    },500);
 
 const apiUrl = 'https://api.waifu.im/search';
 
@@ -17,17 +21,20 @@ fetch(apiUrl, { headers })
   .then(response => {
     if (response.ok) {
       return response.json();
+   
     } else {
       throw new Error('Request failed with status code: ' + response.status);
     }
+  
   })
   .then(data => {
-    console.log(data)
+    //console.log(data)
    let imgWife  = data.images[0].url
    let name = data.images[0].tags[0].name
    let description = data.images[0].tags[0].description
-   console.log(description)
-    data.images[0].width = 150
+   //console.log(description)
+    //data.images[0].width = 100
+    //data.images[0].height = 120
 
    image.src = imgWife
    console.log(imgWife)
@@ -37,7 +44,13 @@ fetch(apiUrl, { headers })
    console.log(wifeDescription)
 
   })
+ 
    .catch(error => {
     console.error('An error occurred:', error.message);
+  
   }); 
  
+  setTimeout (function(){
+
+
+  },2000);
